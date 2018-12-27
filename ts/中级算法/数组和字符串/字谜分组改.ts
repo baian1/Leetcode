77409cddb4cdee2581bd81b8e8095ed5c8947453
baian1 <chizhongnan1@gmail.com>
2018-12-27 17:26:37 +0800

    无重复最长子串

--- ts/中级算法/数组和字符串/字谜分组改.ts
+++ ts/中级算法/数组和字符串/字谜分组改.ts
@@ -3,7 +3,9 @@
  * @param {string[]} strs
  * @return {string[][]}
  * 1.将字符转换为数组26个位置字母，对应位置个数表示字母出现次数
- * 
+ *   返回字符串
+ * 2.使用字符串为key,添加进数组
+ * 3.遍历hashMap对象将属性值做成数组
  */
 const groupAnagrams2 = function(strs:string[]):string[][] {
   let arr=[];
