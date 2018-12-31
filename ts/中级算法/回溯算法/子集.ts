c3d484f0a874e69b2607b9e732425571c0695277
baian1 <chizhongnan1@gmail.com>
2018-12-31 14:56:01 +0800

    子集添加注释

--- ts/中级算法/回溯算法/子集.ts
+++ ts/中级算法/回溯算法/子集.ts
@@ -1,21 +1,23 @@
 /**
  * 
  * @param nums 
+ * 类似树状，第一个结点选不选，第二个结点选不选
+ * 状态的回溯
  */
 const subsets = function(nums:number[]) {
-  let list:number[][]=[];
-  let current:number[]=[];
+  let list:number[][]=[];//存储结果
+  let current:number[]=[];//状态保存
   const getlist=function(nums:number[],start:number,end:number){
     if(start===end){
       list.push([...current]);
       return;
-    }
+    }//约束条件
 
     current.push(nums[start]);
     getlist(nums,start+1,end);
-    current.pop();
+    current.pop();//分支1，选择结点
   
-    getlist(nums,start+1,end);
+    getlist(nums,start+1,end);//分支2，不选择结点
   }
   getlist(nums,0,nums.length);
 };
