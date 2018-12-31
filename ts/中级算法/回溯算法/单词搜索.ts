118459bddc1462af98b0854329e83dcef50bd4ce
baian1 <chizhongnan1@gmail.com>
2018-12-31 15:44:18 +0800

    单词搜索问题记录

--- ts/中级算法/回溯算法/单词搜索.ts
+++ ts/中级算法/回溯算法/单词搜索.ts
@@ -9,6 +9,35 @@ const word="ab";
  * @param word 
  * 1.优化为直接返回布尔值，减少判断
  * 2.优化语句加快运行速度
+ * 
+ * 遇到的问题：
+ * 1.去支做的不好，会导致时间超限，关键在于找到正确的后就结束寻找
+ * 比如
+ * if(!state){getStr(hang+1,lie,wordIndex+1);}
+ * if(!state){getStr(hang-1,lie,wordIndex+1);}
+ * if(!state){getStr(hang,lie+1,wordIndex+1);}
+ * if(!state){getStr(hang,lie-1,wordIndex+1);}
+ * 相较于
+ * getStr(hang+1,lie,wordIndex+1);
+ * getStr(hang-1,lie,wordIndex+1);
+ * getStr(hang,lie-1,wordIndex+1);
+ * getStr(hang,lie+1,wordIndex+1);
+ * 
+ * 会有极大的提升，会在寻找到正确选项后一直回溯到开头，不会再往下
+ * 没有判断的在获取正确答案后，还会往下继续寻找其他答案，遍历完所有答案，这是没有必要的
+ * 
+ * 优化问题
+ * fangxiang.forEach(([a,b])=>{
+ *   if(!state){
+ *     getStr(hang+a,lie+b,wordIndex+1);
+ *   }
+ * });
+ * 这样的语句会比
+ * if(!state){getStr(hang+1,lie,wordIndex+1);}
+ * if(!state){getStr(hang-1,lie,wordIndex+1);}
+ * if(!state){getStr(hang,lie+1,wordIndex+1);}
+ * if(!state){getStr(hang,lie-1,wordIndex+1);}
+ * 直接四连慢，猜测原因是步骤较多，还需要查询数组，解构
  */
 const exist = function(board:(string|null)[][], word:string) {
   let fangxiang=[[0,1],[0,-1],[1,0],[-1,0]];
