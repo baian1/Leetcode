7284c735bd88e44638a7a130c704c3f322157d47
baian1 <chizhongnan1@gmail.com>
2018-12-31 22:55:05 +0800

    颜色分类优化代码

--- ts/中级算法/排序和搜索/颜色分类.ts
+++ ts/中级算法/排序和搜索/颜色分类.ts
@@ -2,6 +2,16 @@
  * 
  * @param nums 
  * 思路，遍历数组，把2往后丢，把0往前丢，遇到0不管
+ * 
+ * 优化：
+ * 1.根据思路改写有点混乱的代码
+ * 
+ * 代码过程：
+ * 1.先是确定开头非0位start与末尾非2位end，遍历指针i，方便换位，进行排序
+ * 2.确定循环条件i<=end(=end是由于end位可能是0，也需要换位)
+ * 3.进入循环，需要寻找i位为0或2需要进行换位操作的
+ * 4.确定结束0位置与开始2位置,i的位置就是自非0位置开始寻找到的非1
+ * 5.开始交换，
  */
 const sortColors = function(nums:number[]) {
   if(nums.length===1){
@@ -9,47 +19,44 @@ const sortColors = function(nums:number[]) {
   }
   let start=0;
   let end=nums.length-1;
-  let i=0
-  while(i<=end){
-    while(nums[end]===2){
-      end--;
-    }
+  let i=0;
+
+  while(nums[start]===0){
+    start++;
+    i=start;
+  }
+  while(nums[i]===1){
+    i++;
+  }
+  while(nums[end]===2){
+    end--;
+  }
+
+  while(true){
     if(i>end){
       break;
     }
-
-    while(nums[i]===2){
-      let current;
-      current=nums[i];
+    if(nums[i]===0){
+      nums[i]=nums[start];
+      nums[start]=0;
+      while(nums[start]===0){
+        start++;
+        i=start;
+      }
+      while(nums[i]===1){
+        i++;
+      }
+    }else{
       nums[i]=nums[end];
-      nums[end]=current;
+      nums[end]=2;
       while(nums[end]===2){
         end--;
       }
-      continue;
-    }
-    while(nums[start]===0){
-      start++;
-      i=start;
-      continue;
-    }
-
-    while(nums[i]===0){
-      let current;
-      current=nums[start];
-      nums[start]=nums[i];
-      nums[i]=current;
-      while(nums[start]===0){
-        start++;
-      }
-      i=start;
-      continue;
-    }
-    
-    while(nums[i]===1){
-      i++;
     }
   }
 };
 
-sortColors([2,2]);
+const start2:any=new Date();
+sortColors([2,0,2,1,1,0,2,1,0,2]);
+const end2:any=new Date();
+console.log(end2-start2);
