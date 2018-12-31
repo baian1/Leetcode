fdadc4da4e22571c822501ab8d3d73c61d768626
baian1 <chizhongnan1@gmail.com>
2018-12-31 22:55:40 +0800

    颜色分类优化代码2

--- ts/中级算法/排序和搜索/颜色分类.ts
+++ ts/中级算法/排序和搜索/颜色分类.ts
@@ -1,7 +1,7 @@
 /**
  * 
  * @param nums 
- * 思路，遍历数组，把2往后丢，把0往前丢，遇到0不管
+ * 思路，遍历数组，把2往后丢，把0往前丢，遇到1不管
  * 
  * 优化：
  * 1.根据思路改写有点混乱的代码
@@ -11,7 +11,7 @@
  * 2.确定循环条件i<=end(=end是由于end位可能是0，也需要换位)
  * 3.进入循环，需要寻找i位为0或2需要进行换位操作的
  * 4.确定结束0位置与开始2位置,i的位置就是自非0位置开始寻找到的非1
- * 5.开始交换，
+ * 5.开始交换，0放到0位置，2放到2位置
  */
 const sortColors = function(nums:number[]) {
   if(nums.length===1){
@@ -20,39 +20,31 @@ const sortColors = function(nums:number[]) {
   let start=0;
   let end=nums.length-1;
   let i=0;
-
-  while(nums[start]===0){
-    start++;
-    i=start;
-  }
-  while(nums[i]===1){
-    i++;
-  }
-  while(nums[end]===2){
-    end--;
-  }
-
   while(true){
+    while(nums[start]===0){
+      start++;
+      i=start;
+    }
+    while(nums[i]===1){
+      i++;
+    }
+    while(nums[end]===2){
+      end--;
+    }//确定启示交换位置
+
     if(i>end){
       break;
-    }
+    }//判断是否结束
+
     if(nums[i]===0){
       nums[i]=nums[start];
       nums[start]=0;
-      while(nums[start]===0){
-        start++;
-        i=start;
-      }
-      while(nums[i]===1){
-        i++;
-      }
+      start++;
     }else{
       nums[i]=nums[end];
       nums[end]=2;
-      while(nums[end]===2){
-        end--;
-      }
-    }
+      end--;
+    }//分别对两种情况进行交换
   }
 };
 
