5e4422b4e3d4bc082a6d386f208b5953dc4225bf
baian1 <chizhongnan1@gmail.com>
2019-01-06 19:46:19 +0800

    跳跃游戏优化2

--- ts/中级算法/动态规划/跳跃游戏.ts
+++ ts/中级算法/动态规划/跳跃游戏.ts
@@ -1,28 +1,26 @@
 /**
  * @param {number[]} nums
  * @return {boolean}
- * 使用数组记录每个位置所能到达的最大位置
+ * 跳跃位置会在0时可能停止，只要判断能不能跳过0点就行了
  */
 const canJump = function(nums:number[]):boolean {
-  let Max=nums.length-1;
-  if(Max===0){
-    return true;
+  let length=nums.length;
+  for(let i=0;i<length-1;i++){
+    if(nums[i]===0){
+      let j=i;
+      while(j>=0){
+        if(nums[j]+j>i){
+          break;
+        }else{
+          j--;
+        }
+      }
+      if(j<0){
+        return false;
+      }
+    }
   }
-  let arr=Array(Max).fill(0);
-  arr[0]=nums[0];
-  if(arr[0]===0){
-    return false;
-  }
-  for(let i=1;i<nums.length;i++){
-    arr[i]=arr[i-1]>nums[i]+i?arr[i-1]:nums[i]+i;//比较这个点和前一个点能到的最大位置，取大的
-    if(arr[i]>=Max){
-      return true;
-    }//能到达尾巴
-    if(arr[i]===i){
-      return false;
-    }//停滞不前了
-  }
-  return false;
+  return true;
 };
 
 canJump([2,0,0]);
