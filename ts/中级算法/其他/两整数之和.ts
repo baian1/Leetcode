adb7e3b8787d808665c8a50032f519aaa7668fe1
baian1 <chizhongnan1@gmail.com>
2019-01-08 22:55:36 +0800

    两整数之和注释

--- ts/中级算法/其他/两整数之和.ts
+++ ts/中级算法/其他/两整数之和.ts
@@ -2,6 +2,8 @@
  * @param {number} a
  * @param {number} b
  * @return {number}
+ * 使用亦或操作将0与1位置相加
+ * 使用与操作表示1与1位置是1，用<<进位表示相加进位
  */
 var getSum = function(a:number, b:number):number {
   if(b===0){
