d35126f728c3c8a6d139e4759b693e6ab8956656
baian1 <chizhongnan1@gmail.com>
2019-01-19 19:45:29 +0800

    逆波兰表达式优化

--- ts/中级算法/其他/逆波兰表达式.ts
+++ ts/中级算法/其他/逆波兰表达式.ts
@@ -2,38 +2,47 @@
  * @param {string[]} tokens
  * @return {number}
  * 使用递归的想法，每次对两个数进行操作
+ * 优化:
+ * 1.使用一次遍历进行解决
+ * 对于逆波兰表达式，如果是数字就进行压栈，遇到符号的时候弹出栈，计算结果并压回去
  */
-const evalRPN = function (tokens: string[]): number {
-  let res: number = 0;
-  let pop = tokens.pop();
-  if (pop === undefined) {
-    return 0;
-  }
-  switch (pop) {
-    case "+":
-      res = evalRPN(tokens) + evalRPN(tokens);
-      break;
-    case "-":
-      let subtractor = evalRPN(tokens);
-      let subtrahend = evalRPN(tokens);
-      res = subtrahend - subtractor;
-      break;
-    case "*":
-      res = evalRPN(tokens) * evalRPN(tokens);
-      break;
-    case "/":
-      let divisor = evalRPN(tokens);
-      let dividend = evalRPN(tokens);
-      let current = dividend / divisor;
-      if(current>=0){
-        res=Math.floor(current);
-      }else{
-        res=Math.ceil(current);
+const evalRPN = function (tokens: string[]): number | string {
+  let stack: number[] = []
+  for (let i of tokens) {
+    if (/[+-/*]$/.test(i)) {
+      if (stack.length < 2) {
+        continue;
       }
-      break;
-    default: return parseInt(pop);
+      let a: number = <number>stack.pop();
+      let b: number = <number>stack.pop();
+      let res = 0;
+      switch (i) {
+        case '+':
+          res = a + b;
+          break;
+        case '-':
+          res = b - a;
+          break;
+        case '*':
+          res = a * b;
+          break;
+        case '/':
+          res = b / a;
+          if (res >= 0) {
+            res = Math.floor(res);
+          } else {
+            res = Math.ceil(res);
+          }
+          break;
+        default:
+          return 'error';
+      }
+      stack.push(res);
+      continue;
+    }
+    stack.push(Number(i));
   }
-  return res;
+  return stack[0];
 };
 
 export { evalRPN };
