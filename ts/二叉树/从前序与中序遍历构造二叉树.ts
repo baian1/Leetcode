4d2923b80efe5deb04bb2e97e63a7d8fe49080a7
baian1 <chizhongnan1@gmail.com>
2019-01-22 23:21:54 +0800

    从前序与中序遍历构造二叉树优化

--- ts/二叉树/从前序与中序遍历构造二叉树.ts
+++ ts/二叉树/从前序与中序遍历构造二叉树.ts
@@ -33,34 +33,28 @@ import { TreeNode } from "./构建二叉树";
  * D B E
  * 
  * 那个范围为一个值的时候，就直接返回结点
+ * 优化:
+ * 构造一个结点，从pre中知道根结点，然后从inorder的一个范围内获取其左右结点
  */
 var buildTree = function (preorder: number[], inorder: number[]) {
-  let node: TreeNode = new TreeNode(preorder[0]);
-  const buildTree = function (preorder: number[], inorder: number[], prestart: number, preend: number, instart: number, inend: number): (TreeNode | null) {
-    let value=preorder[prestart];
-
-    if(instart===inend){
-      return new TreeNode(value);
-    }
-
-    let i=instart;
-    while(value!==inorder[i]&&i<=inend){
-      i++;
-    }
-
-
-    if(i<=inend){
-      let node=new TreeNode(value);
-      node.left=buildTree(preorder,inorder,prestart+1,prestart+i-instart,instart,i-1);
-      node.right=buildTree(preorder,inorder,prestart+i-instart+1,preend,i+1,inend);
-      return node;
-    }else{
+  const buildTree = function (preorder: number[], inorder: number[], prestart: number,instart: number, inend: number): (TreeNode | null) {
+    if(instart>inend){
       return null;
-    }
+    }//这个范围是-1，表示没有结点存在，返回null
+    if(instart===inend){
+      return new TreeNode(preorder[prestart]);
+    }//只有一个结点了，那就是叶结点，返回就ok
+
+    let node=new TreeNode(preorder[prestart]);//创造根结点
+    let i=instart;
+    while(inorder[i]!==preorder[prestart] && i<=inend){
+      i++;
+    }//获取根结点在inorder中的位置，用来分隔左树和右树
+    node.left=buildTree(preorder,inorder,prestart+1,instart,i-1);//创造左树
+    node.right=buildTree(preorder,inorder,prestart+i-instart+1,i+1,inend);//创造右树，这个右树的根结点可以根据 根结点位置(preorder)+左树的长度(inorder) 来获取
+    return node;
   }
-  let leftLength=inorder.indexOf(preorder[0]);
-  node.left=buildTree(preorder,inorder,1,leftLength,0,leftLength-1);
-  node.right=buildTree(preorder,inorder,leftLength+1,preorder.length-1,leftLength+1,inorder.length-1);
+  let node: TreeNode|null = buildTree(preorder,inorder,0,0,inorder.length-1);
   return node;
 };
 
