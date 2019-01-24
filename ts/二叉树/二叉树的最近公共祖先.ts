676c2631f4756ad69b579090167284a53560f0a9
baian1 <chizhongnan1@gmail.com>
2019-01-24 20:49:41 +0800

    二叉树的最近公共祖先

---
+++ ts/二叉树/二叉树的最近公共祖先.ts
@@ -0,0 +1,38 @@
+import { TreeNode } from "./构建二叉树";
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ * 递归写法，不断寻找结点，找到后就返回结点，表示已经找到了
+ * 当一个根节点的左右标识位都是找到的时候，这个根结点就是最近的祖先结点
+ * 
+ */
+var lowestCommonAncestor = function (root: TreeNode, p: TreeNode, q: TreeNode) {
+  const dfs = function (root: TreeNode | null, p: TreeNode, q: TreeNode): TreeNode | null {
+    if (root === null) {
+      return null;
+    }//没找到结点就返回null
+    if (root === p || root === q) {
+      return root;
+    }//找到了结点就返回结点，表示找到了
+    let leftTree = dfs(root.left, p, q);//左树是否找到
+    let rightTree = dfs(root.right, p, q);//右树是否找到
+    if (leftTree && rightTree) {//左右都找到就是根节点了
+      return root;
+    } else {
+      return leftTree === null ? rightTree : leftTree;//一边没找到那就不是根结点，返回找到
+    }
+  }
+  return dfs(root,p,q);
+};
+
+export {lowestCommonAncestor};
