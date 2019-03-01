9bd9661e29ab0476fa115546683193d95d6456cf
baian1 <chizhongnan1@gmail.com>
2019-03-01 17:47:13 +0800

    设计链表改善代码逻辑

--- ts/链表/设计链表.ts
+++ ts/链表/设计链表.ts
@@ -26,18 +26,20 @@ class LinkedList<T> implements LinkedListInterface<T>{
     this.head = null;
     this.length = 0;
   }
+
   getIndex(index: number) {
+    if (index > this.length - 1) {
+      return -1;
+    }
     let i = 0;
     let node = this.head;
     while (node && i < index) {
       node = node.next;
       i++;
     }
-    if (i < index || node === null) {
-      return -1;
-    }
     return node.val;
   }
+
   addAtHead(val: T) {
     let node = new LinkedListNode(val);
     node.next = this.head;
@@ -45,78 +47,73 @@ class LinkedList<T> implements LinkedListInterface<T>{
       this.head.prev = node;
     }
     this.head = node;
+    this.length++;
   }
+
   addAtTail(val: T) {
     let node = this.head;
     let Tail = new LinkedListNode(val);
+
     if (!node) {
       this.head = new LinkedListNode(val);
+      this.length++;
       return;
     }
+
     while (node.next) {
       node = node.next;
     }
     Tail.prev = node;
     node.next = Tail;
+    this.length++;
   }
-  addAtIndex(index: number, val: T) {
-    if (this.head === null) {
-      if (index === 0) {
-        this.head = new LinkedListNode(val);
-        return;
-      }
-    }
-    let i = 0;
-    let node = this.head;
-    while (node && i < index - 1) {
-      node = node.next;
-      i++;
-    }
-    if (node === null) {
-      return;
-    } else {
-      let nextNode = node.next;
-      let newNode = new LinkedListNode(val);
-      if (nextNode === null) {
-        newNode.prev = node;
-        node.next = newNode;
-      } else {
-        nextNode.prev = newNode;
-        newNode.next = nextNode;
 
-        newNode.prev = node;
-        node.next = newNode;
+  addAtIndex(index: number, val: T) {
+    if (index < this.length) {
+      let node = this.head;
+      while (index) {
+        node = node.next;
+        index--;
       }
+      let prenode = node.prev;
+      let newNode = new LinkedListNode(val);
+      prenode.next = newNode;
+      node.prev = newNode;
+      newNode.prev = prenode;
+      newNode.next = node;
+      this.length++;
+    } else if (index === this.length) {
+      this.addAtTail(val);
     }
   }
+
   deleteAtIndex(index: number) {
-    let i = 0;
+    if (index >= this.length || index < 0) {
+      return;
+    }
+
+    if (index === 0) {
+      let node = this.head;
+      this.head = node.next;
+      node.next = null;
+      this.head.prev = null;
+    }
+
     let node = this.head;
-    while (node && i < index) {
+    while (index) {
+      index--;
       node = node.next;
-      i++;
     }
-    if (node) {
-      if (i === 0) {
-        if (node.next === null) {
-          this.head = null;
-        } else {
-          this.head = node.next;
-          node.next.prev = null;
-          node.next = null;
-        }
-      } else {
-        if (node.next === null) {
-          (node.prev as LinkedListNode<T>).next = null;
-          node.prev = null;
-        } else {
-          (node.prev as LinkedListNode<T>).next = node.next;
-          node.next.prev = node.prev;
-          node.next = null;
-          node.prev = null;
-        }
-      }
+    if (node.next === null) {
+      node.prev.next = null;
+      node.prev = null;
+    } else {
+      let pre = node.prev;
+      let next = node.next;
+      pre.next = next;
+      next.prev = pre;
     }
+    this.length--;
   }
 }
 
