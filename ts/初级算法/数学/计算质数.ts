14b6065fe356fa0a4749db8489883629e99fef73
baian1 <chizhongnan1@gmail.com>
2019-04-25 22:29:27 +0800

    计算质数，使用数组存储状态

--- ts/初级算法/数学/计算质数.ts
+++ ts/初级算法/数学/计算质数.ts
@@ -1,22 +1,22 @@
-const isPrimenumber = function(n:number){
-  let arr:number[]=new Array(n+1);
-  let str:string=arr.join('1');
-  let isPrime=/^1$|^(11+?)\1+$/.test(str);//合数返回true;
+const isPrimenumber = function (n: number) {
+  let arr: number[] = new Array(n + 1);
+  let str: string = arr.join('1');
+  let isPrime = /^1$|^(11+?)\1+$/.test(str);//合数返回true;
   return isPrime;
 }
-const countPrimes = function(n:any) {
-  let listPrimne:number[]=[];
-  let vis:{[key: number]:boolean}={};
-  for(let i=2;i<n;i++){
-    if(vis[i]!==true){
-      listPrimne.push(i);
+export const countPrimes = function (n: any) {
+  let notPrime = new Uint8Array(n);
+  let isPrime = [];
+  let count = 0;
+  for (let i = 2; i < n; i++) {
+    if (!notPrime[i]) {
+      count++;
+      isPrime.push(i);
     }
-    for(let j=0;i*listPrimne[j]<=n;j++){
-      vis[i*listPrimne[j]]=true;
-      if(i%listPrimne[j]===0) break;
+    for (let j = 0; i * isPrime[j] <= n; j++) {
+      notPrime[i * isPrime[j]] = 1;
+      if (i % isPrime[j] === 0) break;
     }
   }
-  return listPrimne.join(',');
+  return count;
 };
-
-console.log(countPrimes(100));
