93a3bad1409646c2af9bb59b77211d38462bcd62
baian1 <chizhongnan1@gmail.com>
2019-04-30 15:22:41 +0800

    快速排序补充

--- 基础排序/快速排序.ts
+++ 基础排序/快速排序.ts
@@ -1,9 +1,57 @@
-const sortKuaipai = function (nums: number[], left: number, right: number) {
+/**
+ * 
+ * @param nums 
+ * @param left 
+ * @param right 
+ * 基础快排
+ * Partition
+ * 使用两个指针,
+ * i用来遍历
+ * j用来记录小于等于key的数
+ */
+const swap = (num: number[], i: number, j: number) => {
+  let temp = num[i];
+  num[i] = num[j];
+  num[j] = temp;
+}
+
+export const sortKuaipai = function (nums: number[], left: number, right: number) {
   if (left >= right) {
     return;
   }
-  let i=left;
-  let j=right;
+  swap(nums, left, Math.floor(Math.random() * (right - left + 1) + left));//随机快排
+
+  const key = nums[left];
+  let j = left;
+  for (let i = left + 1; i <= right; i++) {
+    if (nums[i] < key) {
+      j++;
+      swap(nums, j, i);
+    }
+  }
+  swap(nums, left, j);
+  sortKuaipai(nums, left, j - 1);
+  sortKuaipai(nums, j + 1, right);
+}
+
+/**
+ * 
+ * @param nums 
+ * @param left 
+ * @param right 
+ * 双路快排
+ * 设置两个Partition
+ * 用i表示<key
+ * j表示>key
+ */
+export const sortKuaipai2 = function (nums: number[], left: number, right: number) {
+  if (left >= right) {
+    return;
+  }
+  swap(nums, left, Math.floor(Math.random() * (right - left + 1) + left));//随机快排
+
+  let i = left;
+  let j = right;
   let key = nums[i];
   while (i < j) {
     while (key <= nums[j] && i < j) {
@@ -16,8 +64,6 @@ const sortKuaipai = function (nums: number[], left: number, right: number) {
     nums[j] = nums[i];
   }
   nums[i] = key;
-  sortKuaipai(nums, left, i -1);
-  sortKuaipai(nums, i + 1, right);
+  sortKuaipai2(nums, left, i - 1);
+  sortKuaipai2(nums, i + 1, right);
 }
-
-sortKuaipai([4, 2, 5, 3, 6, 7, 3, 5, 7, 8], 0, 9);
