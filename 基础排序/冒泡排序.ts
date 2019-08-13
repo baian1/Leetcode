76ca4eed6c6ac812ccbb7d032f64cd176e9ced73
baian1 <chizhongnan1@gmail.com>
2019-08-13 14:46:25 +0800

    冒泡排序优化

--- 基础排序/冒泡排序.ts
+++ 基础排序/冒泡排序.ts
@@ -1,16 +1,24 @@
+function swap(arr:number[],i:number,j:number){
+  let temp=arr[i]
+  arr[i]=arr[j]
+  arr[j]=temp
+}
+
 /**
  * 冒泡排序,每次循环取到最大的数或最小的数到合适位置
  * @param nums 
  */
 const sortMaopao=function(nums:number[]):void{
   for(let i=0;i<nums.length-1;i++){
+    let didSwap = false;
     for(let j=i+1;j<nums.length;j++){
       if(nums[i]<nums[j]){
-        let cur:number;
-        cur=nums[i];
-        nums[i]=nums[j];
-        nums[j]=cur;
+        swap(nums,i,j)
+        didSwap=true
       }
+    }
+    if(didSwap===false){
+      return
     }
   }
 }
