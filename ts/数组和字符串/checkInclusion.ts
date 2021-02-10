c5f9b56db79f7ec25807677ea058f8af006949f0
fogmonth <chizhongnan1@gmail.com>
2021-02-11 02:29:03 +0800

    数组与字符: 567字符串的排列,优化

--- ts/数组和字符串/checkInclusion.ts
+++ ts/数组和字符串/checkInclusion.ts
@@ -1,38 +1,25 @@
-function isCharSumArrEqual(arr1: number[], arr2: number[]) {
-  for (let i in arr1) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
 function checkInclusion(s1: string, s2: string): boolean {
   if (s1.length > s2.length) {
     return false;
   }
-  const s1CharSum: number[] = new Array(26).fill(0);
+  const charCount: number[] = new Array(26).fill(0);
   for (let char of s1) {
     const index = char.charCodeAt(0) - 97;
-    s1CharSum[index]++;
+    charCount[index]++;
   }
-  let s2SubCharSum: number[] = new Array(26).fill(0);
   for (let i = 0; i < s1.length; i++) {
     const index = s2[i].charCodeAt(0) - 97;
-    s2SubCharSum[index]++;
+    charCount[index]--;
   }
-  if (isCharSumArrEqual(s2SubCharSum, s1CharSum)) {
-    return true;
-  }
+  if (charCount.every((e) => !e)) return true;
   let i = 0;
   let j = s1.length;
   for (; j < s2.length; i++, j++) {
     const startCharIndex = s2[i].charCodeAt(0) - 97;
-    s2SubCharSum[startCharIndex]--;
+    charCount[startCharIndex]++;
     const endCharIndex = s2[j].charCodeAt(0) - 97;
-    s2SubCharSum[endCharIndex]++;
-    if (isCharSumArrEqual(s2SubCharSum, s1CharSum)) {
-      return true;
-    }
+    charCount[endCharIndex]--;
+    if (charCount.every((e) => !e)) return true;
   }
   return false;
 }
