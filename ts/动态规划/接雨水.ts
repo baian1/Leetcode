f969698d544f44c4567778243d8b8e9076d0b51a
fogmonth <chizhongnan1@gmail.com>
2021-02-11 18:01:01 +0800

    动态规划: 接雨水,用栈提升效率

--- ts/动态规划/接雨水.ts
+++ ts/动态规划/接雨水.ts
@@ -3,41 +3,52 @@ function trap(height: number[]): number {
   //每个位置都需要计算左右两侧的最高点
   //那先通过一次遍历计算每个位置最高点
   const dp = new Array(height.length).fill(0);
+  //记录前i中最高的位置
+  //当遇到起伏时
+  //弹出堆栈寻找左边的墙
+  const maxHeight: [number, number][] = [];
   for (let i = 0; i < height.length; i++) {
     if (i === 0) {
       dp[0] = height[0];
+      maxHeight[0] = [height[0], 0];
       continue;
     }
-    if (height[i] > dp[i - 1]) {
+    dp[i] = height[i];
+
+    //往前寻找边，填充空格
+    if (dp[i] > dp[i - 1]) {
       //找到前面最大的数值，与其配合填平
-      dp[i] = height[i];
-      const [max, index] = getMax(i);
+      let maxLine = maxHeight.pop();
+      while (
+        maxLine !== undefined &&
+        maxLine[0] < dp[i] &&
+        maxHeight.length > 0
+      ) {
+        let top = maxHeight.pop()!;
+        if (maxLine[0] < top[0]) {
+          maxLine = top;
+        }
+      }
+      if (maxLine === undefined) {
+        throw new Error("top 不可能为空");
+      }
+      const [max, index] = maxLine;
+      //如果当前边的高较小，需要把前面pop出来的边加回去
+      if (max <= maxLine[0]) {
+        maxHeight.push(maxLine);
+      }
       const maxHeigt = Math.min(max, height[i]);
       for (let start = index + 1; start < i; start++) {
         dp[start] = maxHeigt;
       }
-    } else {
-      dp[i] = height[i];
     }
+
+    maxHeight.push([height[i], i]);
   }
 
   return dp.reduce((sum, dpH, index) => {
     return (sum += dpH - height[index]);
   }, 0);
-  function getMax(end: number) {
-    let max = 0;
-    let index = 0;
-    for (let i = end - 1; i >= 0; i--) {
-      if (height[i] > max) {
-        max = height[i];
-        index = i;
-        if (max >= height[end]) {
-          break;
-        }
-      }
-    }
-    return [max, index];
-  }
 }
 
 export { trap };
