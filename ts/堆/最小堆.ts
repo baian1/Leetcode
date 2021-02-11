ea4f2c108aa2cb5a444457e5a301f837235dbb24
fogmonth <chizhongnan1@gmail.com>
2021-02-11 13:01:58 +0800

    最小堆: 数据流中的第 K 大元素

--- ts/堆/最小堆.ts
+++ ts/堆/最小堆.ts
@@ -1,24 +1,27 @@
 interface HeapStruct<T> {
   Elements: T[];
-  Size: number;//堆当前元素个数
-  Cpacity: number;//最大容量
+  Size: number; //堆当前元素个数
+  Cpacity: number; //最大容量
 }
 
 class MinHeap<T> implements HeapStruct<T> {
   Elements: T[];
-  Size: number;//堆当前元素个数
-  Cpacity: number;//最大容量
+  Size: number; //堆当前元素个数
+  Cpacity: number; //最大容量
   constructor(minValue: T, Cpacity: number) {
     this.Size = 0;
     this.Cpacity = Cpacity;
+    //输入0位置的最小元素,最为最小堆的顶
+    //用于计算过程中递归的条件限制
+    //最小堆从下标1开始
     this.Elements = [minValue];
   }
 
   insert(item: T) {
     if (this.Size === this.Cpacity) {
-      throw new RangeError('堆已满');
+      throw new RangeError("堆已满");
     }
-    let i = ++this.Size;//插入结点起始位置
+    let i = ++this.Size; //插入结点起始位置
     for (; this.Elements[Math.floor(i / 2)] > item; i = Math.floor(i / 2)) {
       this.Elements[i] = this.Elements[Math.floor(i / 2)];
     }
@@ -27,24 +30,24 @@ class MinHeap<T> implements HeapStruct<T> {
 
   delete() {
     if (this.Size === 0) {
-      throw new RangeError('最小堆已空');
+      throw new RangeError("最小堆已空");
     }
-    let MinItem = this.Elements[1];//要删除的结点,根结点最小
-    let temp = this.Elements[this.Size];//获取最后一个结点
+    let MinItem = this.Elements[1]; //要删除的结点,根结点最小
+    let temp = this.Elements[this.Size]; //获取最后一个结点
     this.Elements[1] = temp;
-    this.Size--;//删除后根节点减少一个
-    let Parent = 1;//从根结点开始整理
+    this.Size--; //删除后根节点减少一个
+    let Parent = 1; //从根结点开始整理
     this.sort(Parent);
-    this.Elements.pop();//删除最后结点,用下标Size限制
+    this.Elements.pop(); //删除最后节点,用下标Size限制
     return MinItem;
   }
 
   create(item: T[]) {
     if (this.Size > 0) {
-      throw new Error('已经初始化了');
+      throw new Error("已经初始化了");
     }
     if (item.length > this.Cpacity) {
-      throw new RangeError('超出最大范围');
+      throw new RangeError("超出最大范围");
     }
     this.Size += item.length;
     this.Elements = [...this.Elements, ...item];
@@ -60,7 +63,10 @@ class MinHeap<T> implements HeapStruct<T> {
     let Child;
     for (Parent; Parent * 2 <= this.Size; Parent = Child) {
       Child = Parent * 2;
-      if (Child !== this.Size && this.Elements[Child] > this.Elements[Child + 1]) {
+      if (
+        Child !== this.Size &&
+        this.Elements[Child] > this.Elements[Child + 1]
+      ) {
         Child++;
       }
       if (rootValue < this.Elements[Child]) {
