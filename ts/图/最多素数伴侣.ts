d47504b92fc6ee2ecb6ea2583fbe669490b36c5b
baian1 <chizhongnan1@gmail.com>
2021-03-13 22:09:03 +0800

    图: 素数伴侣

--- ts/图/最多素数伴侣.ts
+++ ts/图/最多素数伴侣.ts
@@ -8,13 +8,13 @@ function isPrimeNumber(num: number) {
 }
 
 /**
- * 素数伴侣
+ * 素数伴侣 匈牙利算法
  * @param inputs
  * @returns
  */
 export function maxPrimeMeta(inputs: number[]) {
-  const odds: number[] = [];
-  const evens: number[] = [];
+  let odds: number[] = [];
+  let evens: number[] = [];
   for (let i = 0; i < inputs.length; i++) {
     let num = inputs[i];
     if (num % 2 === 0) {
@@ -22,6 +22,12 @@ export function maxPrimeMeta(inputs: number[]) {
     } else {
       odds.push(num);
     }
+  }
+
+  if (evens.length < odds.length) {
+    let temp = evens;
+    evens = odds;
+    odds = temp;
   }
 
   const evensUsed: number[] = new Array(evens.length).fill(-1);
@@ -33,39 +39,68 @@ export function maxPrimeMeta(inputs: number[]) {
     }
     return -1;
   }
-
-  for (let i = 0; i < odds.length; i++) {
-    let isMatchEmpty = false;
-    //先寻找空闲的进行匹配
-    for (let j = 0; j < evens.length; j++) {
-      //匹配到空闲
-      if (evensUsed[j] === -1 && isPrimeNumber(odds[i] + evens[j])) {
-        evensUsed[j] = i;
-        isMatchEmpty = true;
-        break;
+  function getAllIsMatched(num: number) {
+    const isMatchedIndex = [];
+    for (let i = 0; i < evens.length; i++) {
+      if (evensUsed[i] !== -1 && isPrimeNumber(num + evens[i])) {
+        isMatchedIndex.push(i);
       }
     }
+    return isMatchedIndex;
+  }
 
-    if (isMatchEmpty) {
+  for (let i = 0; i < odds.length; i++) {
+    //先寻找空闲的进行匹配
+    const evenMatchedIndex = getMatch(odds[i]);
+    if (evenMatchedIndex !== -1) {
+      evensUsed[evenMatchedIndex] = i;
       continue;
     }
 
+    let loop = 0;
+    //当找不到空位置时
+    //需要尝试拆散非空的位置
     for (let j = 0; j < evens.length; j++) {
       if (isPrimeNumber(odds[i] + evens[j])) {
-        const oldMatch = evensUsed[j];
-        if (oldMatch !== -1) {
-          //或取原来odd匹配的，测试有没有其他匹配项
-          const evenIndex = getMatch(odds[oldMatch]);
-          if (evenIndex !== -1) {
-            evensUsed[evenIndex] = oldMatch;
-            evensUsed[j] = i;
-            break;
+        /**
+         * 设置某个位置不被使用
+         * @param index
+         * @returns
+         */
+        const isLock: boolean[] = new Array(evens.length).fill(false);
+        function setEvenUsedEmpty(index: number): boolean {
+          const oddIndex = evensUsed[index];
+          if (oddIndex === -1) {
+            return true;
           }
-        } else {
+          const oddNumber = odds[oddIndex];
+          //先寻找，能不能有空的位置可以直接挪移的
+          const matchedIndex = getMatch(oddNumber);
+          if (matchedIndex !== -1) {
+            evensUsed[matchedIndex] = oddIndex;
+            return true;
+          }
+          //找不到空的位置了，只能让拆分其他匹配对
+          const evensMatchIndex = getAllIsMatched(oddNumber);
+          isLock[index] = true;
+          for (let j of evensMatchIndex) {
+            loop++;
+            if (isLock[j] === false && setEvenUsedEmpty(j)) {
+              evensUsed[j] = oddIndex;
+              return true;
+            }
+          }
+          isLock[index] = false;
+          return false;
+        }
+
+        const isEmpty = setEvenUsedEmpty(j);
+        if (isEmpty) {
           evensUsed[j] = i;
           break;
         }
       }
+      loop = 0;
     }
   }
   return evensUsed.filter((v) => v !== -1).length;
