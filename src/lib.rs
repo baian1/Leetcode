f8fd9415308c940a68e22dfa5ca9cff8959171c7
baian1 <chizhongnan1@gmail.com>
2021-10-12 18:04:53 +0000

    游戏中弱角色的数量

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
+mod medium;
 mod offer;
---
+++ src/medium/m_1996.rs
@@ -0,0 +1,63 @@
+struct Solution();
+
+impl Solution {
+    pub fn number_of_weak_characters(mut properties: Vec<Vec<i32>>) -> i32 {
+        properties.sort_by(|a, b| match a[0].cmp(&b[0]) {
+            std::cmp::Ordering::Less => std::cmp::Ordering::Less.reverse(),
+            std::cmp::Ordering::Greater => std::cmp::Ordering::Greater.reverse(),
+            std::cmp::Ordering::Equal => a[1].cmp(&b[1]),
+        });
+        let mut count = 0;
+        let mut max_def = properties[0][1];
+        for propertie in properties.iter() {
+            if propertie[1] < max_def {
+                count += 1;
+            } else {
+                max_def = propertie[1];
+            }
+        }
+
+        count
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    macro_rules! suzutoaoo {
+        ($a:expr) => {
+            Solution::number_of_weak_characters(Vec::from($a.map(|v| Vec::from(v))))
+        };
+    }
+
+    #[test]
+    fn test_1() {
+        assert_eq!(suzutoaoo!([[5, 5], [6, 3], [3, 6]]), 0);
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(suzutoaoo!([[5, 5], [6, 3], [3, 6]]), 0);
+    }
+
+    #[test]
+    fn test_3() {
+        assert_eq!(
+            suzutoaoo!([
+                [7, 7],
+                [1, 2],
+                [9, 7],
+                [7, 3],
+                [3, 10],
+                [9, 8],
+                [8, 10],
+                [4, 3],
+                [1, 5],
+                [1, 5]
+            ]),
+            6
+        );
+    }
+}
