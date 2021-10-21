e6ded14fa013e092b64c5e4ce3af4d6dcbd4d17c
baian1 <chizhongnan1@gmail.com>
2021-10-21 16:05:08 +0000

    easy_453

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::ops::Add;
+
+#[derive(Debug)]
+struct Complex<T: Add> {
+    real: T,
+    imag: T,
+}
+
+impl<T> Add for Complex<T>
+where
+    T: Add<Output = T>,
+{
+    type Output = Complex<T>;
+    fn add(self, rhs: Self) -> Self::Output {
+        Complex {
+            real: self.real + rhs.real,
+            imag: self.imag + rhs.imag,
+        }
+    }
+}
+
 fn main() {
+    dbg!(
+        Complex {
+            real: (1, 2),
+            imag: (2, 1)
+        } + Complex {
+            real: (1, 1),
+            imag: (1, 2)
+        }
+    );
     println!("Hello, world!");
 }
