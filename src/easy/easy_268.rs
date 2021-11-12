0a5fa589c8885537e85ddfaa061f67ae4c45d0dc
baian1 <chizhongnan1@gmail.com>
2021-11-12 16:49:30 +0000

    easy268

---
+++ src/easy/easy_268.rs
@@ -0,0 +1,18 @@
+struct Solution();
+impl Solution {
+    pub fn missing_number(nums: Vec<i32>) -> i32 {
+        nums.iter()
+            .enumerate()
+            .fold(0, |sum, (index, value)| sum + index as i32 + 1 - value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(Solution::missing_number(vec![3, 0, 1]), 2)
+    }
+}
--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -1,4 +1,5 @@
 mod easy_1417;
+mod easy_268;
 mod easy_453;
 mod easy_476;
 mod easy_598;
