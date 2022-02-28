f899076cb1070c407304f39d276abc233f198385
baian1 <chizhongnan1@gmail.com>
2022-03-01 00:45:53 +0800

    offer_24

---
+++ src/offer/offer_24.rs
@@ -0,0 +1,35 @@
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+pub struct Solution();
+impl Solution {
+    pub fn reverse_list(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut cur = None;
+        loop {
+            if let Some(mut v) = head {
+                head = v.next.take();
+                v.next = cur;
+                cur = Some(v);
+            } else {
+                break;
+            }
+        }
+        cur
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn test_1() {}
+}
