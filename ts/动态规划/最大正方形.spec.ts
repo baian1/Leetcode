1c4a4822ac6c3696223f787fa1d7e93d98b578d0
baian1 <chizhongnan1@gmail.com>
2022-04-13 01:50:11 +0800

    最大正方形优化: 一维数组

--- ts/动态规划/最大正方形.spec.ts
+++ ts/动态规划/最大正方形.spec.ts
@@ -3,34 +3,29 @@ import { expect } from "chai";
 function maximalSquare(matrix: string[][]): number {
   let length = matrix[0].length;
   // 当前位置,往左上找到的最大正方形
-  let dp: number[][] = [];
-  for (let i = 0; i < matrix.length; i++) {
-    dp.push(new Array(length).fill(0));
-  }
+  let dp: number[] = new Array(length).fill(0);
+
   let max = 0;
-  for (let j = 0; j < matrix.length; j++) {
-    const row = matrix[j];
+  for (let row of matrix) {
+    // 左上角的点
+    let lt = 0;
     for (let i = 0; i < length; i++) {
+      let cur = dp[i];
       if (row[i] === "1") {
         // 左,左上,上 => 最小值 + 1
-        dp[j][i] = getMin(dp, j, i) + 1;
+        dp[i] = Math.min(Math.min(getV(dp, i), getV(dp, i - 1)), lt) + 1;
       } else if (row[i] === "0") {
-        dp[j][i] = 0;
+        dp[i] = 0;
       }
+      lt = cur;
 
-      max = Math.max(max, dp[j][i]);
+      max = Math.max(max, dp[i]);
     }
   }
 
   return Math.pow(max, 2);
-  function getV(matrix: number[][], i: number, j: number) {
-    return matrix[i]?.[j] || 0;
-  }
-  function getMin(dp: number[][], i: number, j: number) {
-    return Math.min(
-      Math.min(getV(dp, i - 1, j), getV(dp, i, j - 1)),
-      getV(dp, i - 1, j - 1)
-    );
+  function getV(dp: number[], i: number) {
+    return dp[i] || 0;
   }
 }
 
